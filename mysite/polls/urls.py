8e2a5648013a2a75306b84a2e031af88a8f72814
Russell Goldman <russellgoldman1@gmail.com>
2018-05-16 21:47:17 -0400

    Created first Django template with Jinja on /polls/Question.id

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    # creating a path for the 'index' view (127.0.0.1/polls/)
-    path('', views.index, name="index")
+    # creating a path for the 'index' view
+    # e.g. 127.0.0.1/polls/
+    path('', views.index, name='index'),
+
+    # int specifies the parameter type and question_id is the parameter to show in PATH
+    # e.g. 127.0.0.1/polls/1
+    path('<int:question_id>/', views.detail, name='detail'),
+
+    # question_id is the parameter to show in PATH, and 'results' is the hardcoded route after it
+    # e.g. 127.0.0.1/polls/1/vote
+    path('<int:question_id>/results/', views.results, name='results'),
+
+    # e.g. 127.0.0.1/polls/1/results
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,8 +2,37 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Question
+
 # Create your views here.
 
+
 def index(request):
-    # returning an HttpResponse to render to the page
-    return HttpResponse('Awesome job guys! This is the index page, of our polls application')
+    # Question.objects.order_by() returns a QuerySet object containing the Question instances
+    # '-pub_date' selects by publication date
+    # [:5] tells Django to select the objects from 0 to 5 (first 5 objects) from the query
+    # If we did [5:], this would tell us to select all objects (including and after) the 5th index in the QuerySet
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # For each Question instance in latest_question_list, extract each instances question_text attribute
+    # and separate them by a comma (except for the last item)
+    # e.g. output = ', '.join(q.question_text for q in latest_question_list)
+
+    context = {'latest_question_list': latest_question_list}
+
+    # rendering our output to the page by passing in our HTML file and our required parameters as context
+    return render(request, 'polls/index.html', context)
+
+
+# whatever value is in <int:question_id> in the route will be the value stored in question_id below
+def detail(request, question_id):
+    # rendering text with a parameter
+    return HttpResponse("This is the detail view of the question: %s" % question_id)
+
+
+def results(request, question_id):
+    return HttpResponse("These are the results of the question: %s" % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("Vote on question: %s" % question_id)
